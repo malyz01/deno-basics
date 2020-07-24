94c3d70885356f6806a40171fba90ddc345e199d
malyz <mattuy001@gmail.com>
2020-07-25 09:07:23 +1200

    NEW: initial commit

---
+++ http/server.ts
@@ -0,0 +1,16 @@
+import {
+  serve,
+  Server,
+  ServerRequest
+} from 'https://deno.land/std@0.62.0/http/server.ts'
+
+const s: Server = serve({ port: 8000 })
+console.log('Started at port 8000')
+
+for await (const req: ServerRequest of s) {
+  if (req.url === '/') {
+    req.respond({ body: 'Helllllooo' })
+  } else {
+    req.respond({ body: 'Some other url' })
+  }
+}
